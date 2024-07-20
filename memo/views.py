27c8d6f5621f7cdaa2717d105e5851d9dcda797d
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-20 19:55:14 -0400

    feat: views de formularios

--- memo/views.py
+++ memo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import MemorandumForm, TipoMemoForm  # Importa los formularios necesarios
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def formulario_memorandum(request):
+    if request.method == "POST":
+        form = MemorandumForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(
+                "success"
+            )  # Redirige a una página de éxito o a donde quieras
+    else:
+        form = MemorandumForm()
+    return render(request, "formulario.html", {"form": form})
+
+
+@login_required
+def formulario_tipo_memo(request):
+    if request.method == "POST":
+        form = TipoMemoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(
+                "success"
+            )  # Redirige a una página de éxito o a donde quieras
+    else:
+        form = TipoMemoForm()
+    return render(request, "formulario.html", {"form": form})
