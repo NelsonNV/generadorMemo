8ac5417a298bf0e8f05dc35e77a60b7d2cb20638
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-20 15:43:52 -0400

    feat: formulario

---
+++ memo/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from memo.models import Memorandum, Tipo_memo, Documento, User
+
+
+class BaseModelForm(forms.ModelForm):
+    class Meta:
+        abstract = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+
+class MemorandumForm(forms.ModelForm):
+    copia_destinatario = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,  # Permite que este campo esté vacío
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Memorandum
+        fields = [
+            "destinatario",
+            "remitente",
+            "copia_destinatario",
+            "asunto",
+            "fecha",
+            "cuerpo",
+            "cierre",
+            "tipo",
+        ]
+
+
+class TipoMemoForm(BaseModelForm):
+    class Meta(BaseModelForm.Meta):
+        model = Tipo_memo
+        fields = ["nombre", "formato"]
+
+
+class DocumentoForm(BaseModelForm):
+    class Meta(BaseModelForm.Meta):
+        model = Documento
+        fields = ["documento"]
